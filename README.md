af13794cbc34daf0fcf299d30eaeb6ad69447c7e
MadEasterBunny <40764200+MadEasterBunny@users.noreply.github.com>
2026-01-27 00:26:06 +0900

    Initialize project

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # memory-card-top
+
 A memory card game created using React through The Odin Project
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './styles/App.css'
+import Game from './components/Game/Game'
+
+function App() {
+
+  return (
+    <Game />
+  )
+}
+
+export default App
