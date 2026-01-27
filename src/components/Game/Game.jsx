fafcbc48ac3c0dc503a48566e148faf710cbd1b2
MadEasterBunny <40764200+MadEasterBunny@users.noreply.github.com>
2026-01-27 23:08:49 +0900

    Refactor the fetch function to the Game component to avoid prop drilling onClick event to Card component

--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -1,12 +1,38 @@
-
-import { useState } from "react"
-import Gameboard from "../Gameboard/Gameboard"
+import { useState, useEffect } from "react"
 import Scoreboard from "../Scoreboard/Scoreboard"
+import Gameboard from "../Gameboard/Gameboard"
+import Card from "../Card/Card"
 
 export default function Game() {
+    const [pokemon, setPokemon] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
     const [prevClick, setPrevClick] = useState([]);
     const [score, setScore] = useState(0);
     const [highScore, setHighScore] = useState(0);
+
+    useEffect(() => {
+        const fetchPokemon = async () => {
+            try {
+                const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=18');
+
+                if(!response.ok) {
+                    throw new Error(`HTTP error! Status: ${response.status}`);
+                }
+
+                const data = await response.json();
+                setPokemon(data.results);
+                setError(null);
+            } catch (error) {
+                setError(`Error fetching data: ${error.message}`);
+                setPokemon([]);
+            } finally {
+                setLoading(false);
+            }
+        }
+
+        fetchPokemon();
+    }, []);
 
     const handleClick = (data) => {
         if(prevClick.includes(data)) {
@@ -19,6 +45,8 @@ export default function Game() {
             return;
         }
 
+        console.log(`Curr click: ${data}`);
+        console.log(`Prev click: ${prevClick}`);
         setPrevClick([...prevClick, data]);
         setScore(score + 1);
     }
@@ -32,7 +60,19 @@ export default function Game() {
                 </div>
                 <Scoreboard score={score} highScore={highScore} />
             </div>
-            <Gameboard onClickCard={handleClick} />
+            {error && <div>{error}</div>}
+            {loading && <div>Loading cards...</div>}
+            {!loading && !error && (
+                <Gameboard>
+                    {pokemon.map((pokemon, index) => {
+                        const pokemonId = index + 1;
+                        const imageUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonId}.png`;
+                        return(
+                            <Card key={pokemon.name} img={imageUrl} text={pokemon.name} onClickCard={() => handleClick(pokemon.name)}/>
+                        )
+                    })}
+                </Gameboard>
+            )}
         </>
     )
 }
--- src/components/Gameboard/Gameboard.jsx
+++ src/components/Gameboard/Gameboard.jsx
@@ -1,48 +1,9 @@
-import { useEffect, useState } from "react"
 import styles from './Gameboard.module.css'
-import Card from "../Card/Card"
 
-export default function Gameboard({ onClickCard }) {
-    const [pokemon, setPokemon] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
-
-    useEffect(() => {
-        const fetchPokemon = async () => {
-            try {
-                const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=18');
-
-                if(!response.ok) {
-                    throw new Error(`HTTP error! Status: ${response.status}`);
-                }
-
-                const data = await response.json();
-                setPokemon(data.results);
-                setError(null);
-            } catch (error) {
-                setError(`Error fetching data: ${error.message}`);
-                setPokemon([]);
-            } finally {
-                setLoading(false);
-            }
-        }
-
-        fetchPokemon();
-    }, []);
-
-    if(loading) return <div>Loading cards...</div>
-
-    if(error) return <div>{error}</div>
-
+export default function Gameboard({ children }) {
     return(
         <div className={styles.gameboardWrapper}>
-            {pokemon.map((pokemon, index) => {
-                const pokemonId = index + 1;
-                const imageUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonId}.png`;
-                return(
-                    <Card key={pokemon.name} img={imageUrl} text={pokemon.name} onClickCard={() => onClickCard(pokemon.name)}/>
-                )
-            })}
+            {children}
         </div>
     )
 }
